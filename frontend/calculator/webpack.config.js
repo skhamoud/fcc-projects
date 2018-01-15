90889217c5bead517688d14b65bb999d75e963ab
Hamoud Khatar <skhamoud@gmail.com>
2018-01-15 18:35:42 +0000

    webpack and dev server

---
+++ frontend/calculator/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './build/',
+    compress: true,
+    port: 5000,
+    open: true,
+  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/,
+  //       use: ["style-loader", "css-loader"]
+  //     }
+  //     // {
+  //     //   test: /\.(png|svg|jpg|gif)$/,
+  //     //   use: ["file-loader"]
+  //     // }
+  //   ]
+  // }
+};
